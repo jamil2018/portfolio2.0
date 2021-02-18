7658c1cc44466980d4239fc5b8ef9e84b8180dc0
Hasnat Jamil <hasnatjamil2018@gmail.com>
2021-02-19 04:02:50 +0600

    changeset: 1. implement sticky navbar menu 2. implement section highlighting on sticky navbar menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,30 @@
-const hello = () => {
-  return console.log("hello world");
-};
+const mainNav = document.querySelector("#headerNav");
+const navbarToggleTriggerSection = document.querySelector(
+  "section:nth-of-type(1)"
+);
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", () => {
+  if (this.scrollY + 50 >= navbarToggleTriggerSection.offsetTop)
+    mainNav.classList.add("scroll-header");
+  else mainNav.classList.remove("scroll-header");
+});
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.header__links a[href*=${sectionId}]`)
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(`.header__links a[href*=${sectionId}]`)
+        .classList.remove("active");
+    }
+  });
+});
